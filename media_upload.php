b2d690ec3c0d1e9668622030657f3cbaf1393ab3
Draco705 <jmg705@gmail.com>
2015-04-20 20:02:42 -0400

    Changed media upload page to fit CSS style

--- media_upload.php
+++ media_upload.php
@@ -6,6 +6,7 @@ session_start();
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Media Upload</title>
+<link rel="stylesheet" type="text/css" href=css/default.css>
 </head>
 
 <body>
@@ -15,14 +16,14 @@ session_start();
   <p style="margin:0; padding:0">
   <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
    Add a Media: <label style="color:#663399"><em> (Each file limit 10M)</em></label><br/>
-   <input  name="file" type="file" size="50" /> <br><br>
+   <input  name="file" type="file" class="button"  size="50" /> <br><br>
    
 Title*: <input type="text" name="title" required> <br><br>
 Description:
 <textarea rows="5" cols="50" name="description" placeholder="Description about file">
 </textarea> <br>
   
-	<input value="Upload" name="submit" type="submit" />
+	<input value="Upload" name="submit" class="button"  type="submit" />
   </p>
  
  
